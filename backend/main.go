a41ec3c9d08d05bf66544b73389e9aa98046f0de
dmcmcmahon714 <57333568+dmcmcmahon714@users.noreply.github.com>
2020-06-27 15:37:54 -0500

    define reader and print message

--- backend/main.go
+++ backend/main.go
@@ -15,6 +15,21 @@ WriteBufferSize: 1024,
 CheckOrigin: func(r *http.Request) bool { return true },
 }
 
+func reader(conn *websocket.Conn) {
+	for {
+		messageType, p, err := conn.ReadMessage()
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		fmt.Println(string(p))
+		if err := conn.WriteMessage(messageType, p); err != nil {
+			log.Prinln(err)
+			return
+		}
+	}
+}
+
 func setupRoutes(){
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
 		fmt.Fprintf(w, "Simple Server")
