4e6b2587c6d71d9b2983323d85ae92acd78c494b
dmcmcmahon714 <57333568+dmcmcmahon714@users.noreply.github.com>
2020-06-27 15:47:59 -0500

    debugged main.go

--- backend/main.go
+++ backend/main.go
@@ -22,30 +22,41 @@ func reader(conn *websocket.Conn) {
 			log.Println(err)
 			return
 		}
+
 		fmt.Println(string(p))
-		if err := conn.WriteMessage(messageType, p); err != nil {
-			log.Prinln(err)
-			return
-		}
-	}
+
+        if err := conn.WriteMessage(messageType, p); err != nil {
+            log.Println(err)
+            return
+        }
+
+    }
 }
 
+
 func serveWs(w http.ResponseWriter, r *http.Request) {
-	fmt.Println(r.Host)
-	ws, err := upgrader.Upgrade(w, r, nil)
-	if err != nil {
-		log.Prinln(err)
-	}
-	reader(ws)
+    fmt.Println(r.Host)
+
+  // upgrade this connection to a WebSocket
+  // connection
+    ws, err := upgrader.Upgrade(w, r, nil)
+    if err != nil {
+        log.Println(err)
+  }
+  // listen indefinitely for new messages coming
+  // through on our WebSocket connection
+    reader(ws)
 }
 
 func setupRoutes(){
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
 		fmt.Fprintf(w, "Simple Server")
 	})
+	http.HandleFunc("/ws", serveWs)
 }
 
 func main() {
-	setupRoutes()
-  http.ListenAndServe(":8080", nil)
+    fmt.Println("Chat App v0.01")
+    setupRoutes()
+    http.ListenAndServe(":8080", nil)
 }
